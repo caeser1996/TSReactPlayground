a82da3033b7e9768ce1d042cdcc062c3da282ef2
caeser1996 <sumanta9090@gmail.com>
2025-02-02 17:02:34 +0530

    - Added more to array types.

--- ts/arrayTypes.ts
+++ ts/arrayTypes.ts
@@ -1,7 +1,33 @@
 // Array Types
+// What is Array<any>
+// Array<any> is a type that represents the collection of any type elements.
 // Array is a type that represents the collection of elements. It is a type that represents the array type values.
 // Syntax:
 // let variableName:type[];
 // Example:
 let arr:number[]=[10,20,30];
 console.log(arr);
+
+// What is Array<string>
+// Array<string> is a type that represents the collection of string elements.
+let arr1:string[]=["Hello","World"];
+
+// What is Array<number>
+// Array<number> is a type that represents the collection of number elements.
+let arr2:number[]=[10,20,30];
+
+// What is Array<boolean>
+// Array<boolean> is a type that represents the collection of boolean elements.
+let arr3:boolean[]=[true,false];
+
+// What is Array<undefined>
+// Array<undefined> is a type that represents the collection of undefined elements.
+let arr4:undefined[]=[undefined,undefined];
+
+// What is Array<null>
+// Array<null> is a type that represents the collection of null elements.
+let arr5:null[]=[null,null];
+
+// What is Array<object>
+// Array<object> is a type that represents the collection of object elements.
+let arr6:object[]=[{name:"John",age:30},{name:"Smith",age:40}];
